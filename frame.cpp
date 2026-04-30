bed2d556bbf8b51232227d58d925514de36dd1a1
lumnhucc <25521312@gm.uit.edu.vn>
2026-04-30 23:26:07 +0700

    thêm màu sắc và fix độ nhạy nút điều khiển

--- frame.cpp
+++ frame.cpp
@@ -1,92 +1,80 @@
 #include <iostream>
-#include <unistd.h>
-#include <termios.h>
-#include <fcntl.h>
 #include <ctime>
 #include <cstdlib>
+#ifdef _WIN32
+    #include <windows.h>
+    #include <conio.h>
+#else
+    #include <unistd.h>
+    #include <termios.h>
+    #include <fcntl.h>
+#endif
 
 using namespace std;
 #define H 20
 #define W 15
+
+#define RESET   "\033[0m"
+#define CYAN    "\033[36m"
+#define YELLOW  "\033[33m"
+#define MAGENTA "\033[35m"
+#define GREEN   "\033[32m"
+#define RED     "\033[31m"
+#define BLUE    "\033[34m"
+#define WHITE   "\033[37m"
+
 char board[H][W] = {} ;
 char blocks[][4][4] = {
-        {{' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '}},
-        {{' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'I','I','I','I'},
-         {' ',' ',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','T',' ',' '},
-         {'T','T','T',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','S','S',' '},
-         {'S','S',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'Z','Z',' ',' '},
-         {' ','Z','Z',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'J',' ',' ',' '},
-         {'J','J','J',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ',' ','L',' '},
-         {'L','L','L',' '},
-         {' ',' ',' ',' '}}
+        {{' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}},
+        {{' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {'I','I','I','I'}, {' ',' ',' ',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','T',' ',' '}, {'T','T','T',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','S','S',' '}, {'S','S',' ',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {'Z','Z',' ',' '}, {' ','Z','Z',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {'J',' ',' ',' '}, {'J','J','J',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ',' ','L',' '}, {'L','L','L',' '}, {' ',' ',' ',' '}}
 };
 
 int x=4,y=0,b=1;
 
-// --- CÁC HÀM HỖ TRỢ CHUẨN LINUX ---
-void gotoxy(int x, int y) {
-    cout << "\033[" << y + 1 << ";" << x + 1 << "H";
+void applyColor(char c) {
+    switch(c) {
+        case 'I': cout << CYAN; break;
+        case 'O': cout << YELLOW; break;
+        case 'T': cout << MAGENTA; break;
+        case 'S': cout << GREEN; break;
+        case 'Z': cout << RED; break;
+        case 'J': cout << BLUE; break;
+        case 'L': cout << WHITE; break;
+        default: cout << RESET;
+    }
 }
 
-int kbhit(void) {
+void gotoxy(int x, int y) {
+#ifdef _WIN32
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+#else
+    cout << "\033[" << y + 1 << ";" << x + 1 << "H";
+#endif
+}
+
+int check_kbhit(void) {
+#ifdef _WIN32
+    return _kbhit();
+#else
     struct termios oldt, newt;
-    int ch;
-    int oldf;
+    int ch, oldf;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
@@ -96,14 +84,15 @@ int kbhit(void) {
     ch = getchar();
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     fcntl(STDIN_FILENO, F_SETFL, oldf);
-    if(ch != EOF) {
-        ungetc(ch, stdin);
-        return 1;
-    }
+    if(ch != EOF) { ungetc(ch, stdin); return 1; }
     return 0;
+#endif
 }
 
-char getch(void) {
+char get_input(void) {
+#ifdef _WIN32
+    return _getch();
+#else
     char ch;
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
@@ -113,13 +102,21 @@ char getch(void) {
     ch = getchar();
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return ch;
+#endif
 }
-// -----------------------------------
+
+void sleep_ms(int ms) {
+#ifdef _WIN32
+    Sleep(ms);
+#else
+    usleep(ms * 1000); 
+#endif
+}
 
 void boardDelBlock(){
     for (int i = 0 ; i < 4 ; i++)
         for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' && y+j < H)
+            if (blocks[b][i][j] != ' ' && y+i < H)
                 board[y+i][x+j] = ' ';
 }
 
@@ -137,32 +134,31 @@ void initBoard(){
             else board[i][j] = ' ';
 }
 
-// --- HÀM VẼ ĐÃ ĐƯỢC CẬP NHẬT ĐỂ THÊM KHUNG ---
 void draw(){
     gotoxy(0,0);
-    
-    // Vẽ viền trên cùng (nắp đậy của game) và Viền trên của khung điều khiển
-    cout << "╔═";
+    cout << WHITE << "╔═";
     for (int j = 1; j < W - 1; j++) cout << "══";
     cout << "═╗   ╔══════════════════╗" << endl;
 
     for (int i = 0 ; i < H ; i++){
-        // Vẽ khu vực bảng Game
         for (int j = 0 ; j < W ; j++){
             if (board[i][j] == '#') {
+                cout << WHITE;
                 if (i == H - 1 && j == 0) cout << "╚═";
                 else if (i == H - 1 && j == W - 1) cout << "═╝";
                 else if (i == H - 1) cout << "══";
                 else if (j == 0) cout << "║ ";
                 else if (j == W - 1) cout << " ║";
+                cout << RESET;
             } else if (board[i][j] == ' ') {
                 cout << "  "; 
             } else {
-                cout << "[]"; 
+                // ÁP DỤNG MÀU CHO KHỐI
+                applyColor(board[i][j]);
+                cout << "[]" << RESET; 
             }
         }
         
-        // Vẽ khung bảng điều khiển bên phải
         if (i == 0)      cout << "   ║  BẢNG ĐIỀU KHIỂN ║";
         else if (i == 1) cout << "   ╠══════════════════╣";
         else if (i == 2) cout << "   ║ [A] : Sang trái  ║";
@@ -174,7 +170,6 @@ void draw(){
         cout << endl;
     }
 }
-// ----------------------------------------------
 
 bool canMove(int dx, int dy){
     for (int i = 0 ; i < 4 ; i++)
@@ -191,14 +186,14 @@ bool canMove(int dx, int dy){
 void removeLine(){
     int j;
     for (int i = H-2; i >0 ; i-- ){
-        for (j = 0; j < W-1 ; j++)
+        for (j = 1; j < W-1 ; j++)
             if (board[i][j] == ' ') break;
         if (j == W-1){
             for (int ii = i; ii >0 ; ii-- )
-                for (int j = 0; j < W-1 ; j++ ) board[ii][j] = board[ii-1][j];
+                for (int jj = 1; jj < W-1 ; jj++ ) board[ii][jj] = board[ii-1][jj];
             i++;
             draw();
-            usleep(200000); 
+            sleep_ms(100); 
         }
     }
 }
@@ -206,27 +201,38 @@ void removeLine(){
 int main()
 {
     srand(time(0));
-    b = rand() % 7;
-    system("clear"); 
+    b = rand() % 16; 
     initBoard();
+    
+    int moveTimer = 0;
+    int dropLimit = 6; 
+
     while (1){
         boardDelBlock();
-        if (kbhit()){
-            char c = getch();
-            if (c=='a' && canMove(-1,0)) x--;
-            if (c=='d' && canMove(1,0) ) x++;
-            if (c=='x' && canMove(0,1))  y++;
-            if (c=='q') break;
+        
+        if (check_kbhit()){
+            char c = get_input();
+            if ((c=='a' || c=='A') && canMove(-1,0)) x--;
+            if ((c=='d' || c=='D') && canMove(1,0) ) x++;
+            if ((c=='x' || c=='X') && canMove(0,1))  y++;
+            if (c=='q' || c=='Q') break;
         }
-        if (canMove(0,1)) y++;
-        else {
-            block2Board();
-            removeLine();
-            x = 5; y = 0; b = rand() % 7;
+
+        moveTimer++;
+        if (moveTimer >= dropLimit) {
+            if (canMove(0,1)) y++;
+            else {
+                block2Board();
+                removeLine();
+                x = 4; y = 0; b = rand() % 16;
+                if (!canMove(0,0)) break; 
+            }
+            moveTimer = 0;
         }
+
         block2Board();
         draw();
-        usleep(200000); 
+        sleep_ms(30);
     }
     return 0;
 }
