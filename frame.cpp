acf8af85ab16889176f12d1e6cbc3760903c8561
Brimlance <25521823@gm.uit.edu.vn>
2026-05-07 13:20:54 +0700

    sửa thuật toán random bên c++

--- frame.cpp
+++ frame.cpp
@@ -308,17 +308,28 @@ void hardDrop() {
     }
 }
 
+int bag[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+int bagIndex = 8; 
+
 void spawnBlock() {
-    x = 4; 
-    y = 0; 
-    b = rand() % 8;
+    x = 4; y = 0;
+    if (bagIndex >= 8) {
+        for(int i=0; i<8; i++) bag[i] = i;
+        for (int i = 7; i > 0; i--) {
+            int j = rand() % (i + 1);
+            swap(bag[i], bag[j]);
+        }
+        bagIndex = 0;
+    }
+
+    b = bag[bagIndex++];
+    
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             currentBlock[i][j] = blocks[b][i][j];
         }
     }
 }
-
 int main() {
     // Đối với Windows, cần thiết lập mã UTF-8 để hiển thị các ký tự khung
 #ifdef _WIN32
