32e68fd7c0469393bb6652ee8987369096f4fb66
Trần Hoài Linh <25521008@gm.uit.edu.vn>
2026-05-13 20:50:11 +0700

    viet cac ham o CRenderer.cpp va CRenderer.h

--- tetris_skeleton/tetris_skeleton/CRenderer.cpp
+++ tetris_skeleton/tetris_skeleton/CRenderer.cpp
@@ -2,36 +2,129 @@
 //  CRenderer.cpp  –  Người làm: Linh
 // ╚══════════════════════════════════════════╝
 #include "CRenderer.h"
+#include <iostream>
 
 void CRenderer::gotoxy(int col, int row) {
-    // TODO
+#ifdef _WIN32
+    COORD coord;
+    coord.X = col;
+    coord.Y = row;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+#else
+    std::cout << "\033[" << row + 1 << ";" << col + 1 << "H";
+#endif
 }
 
 void CRenderer::applyColor(char c) {
-    // TODO
+    switch(c) {
+        case 'I': std::cout << CYAN; break;
+        case 'O': std::cout << YELLOW; break;
+        case 'T': std::cout << MAGENTA; break;
+        case 'S': std::cout << GREEN; break;
+        case 'Z': std::cout << RED; break;
+        case 'J': std::cout << BLUE; break;
+        case 'L': std::cout << WHITE; break;
+        default: std::cout << RESET;
+    }
 }
 
 std::string CRenderer::padLeft(int val, int width) {
-    // TODO
-    return "";
+    std::string s = std::to_string(val);
+    while ((int)s.size() < width) s = " " + s;
+    return s;
 }
 
 void CRenderer::draw(const CBoard& board,
                      const CBlock& nextBlock,
                      const CScoreManager& score,
                      bool paused) const {
-    // TODO
-    // Gợi ý cấu trúc:
-    //   gotoxy(0, 0);
-    //   in dòng khung trên
-    //   for (int i = 0; i < BOARD_H; i++) {
-    //       vẽ cột trái  (board.grid)
-    //       vẽ cột giữa  (bảng điều khiển + điểm)
-    //       vẽ cột phải  (nextBlock preview)
-    //       cout << "\n";
-    //   }
+    gotoxy(0, 0);
+    // Vẽ khung trên cùng
+    std::cout << WHITE << "╔═";
+    for (int j = 1; j < BOARD_W - 1; j++) std::cout << "══";
+    std::cout << "═╗   ╔══════════════════════╗   ╔══════════╗" << RESET << "\n";
+
+    for (int i = 0 ; i < BOARD_H ; i++){
+        // Vẽ bàn cờ (Trái)
+        for (int j = 0 ; j < BOARD_W ; j++){
+            if (board.grid[i][j] == '#') {
+                std::cout << WHITE;
+                if (i == BOARD_H - 1 && j == 0) std::cout << "╚═";
+                else if (i == BOARD_H - 1 && j == BOARD_W - 1) std::cout << "═╝";
+                else if (i == BOARD_H - 1) std::cout << "══";
+                else if (j == 0) std::cout << "║ ";
+                else if (j == BOARD_W - 1) std::cout << " ║";
+                std::cout << RESET;
+            } else if (board.grid[i][j] == ' ') {
+                std::cout << "  ";
+            } else {
+                applyColor(board.grid[i][j]);
+                std::cout << "[]" << RESET;
+            }
+        }
+
+        // Vẽ Bảng Điều Khiển (Giữa)
+        std::cout << "   "; 
+        switch(i) {
+            case 0:  std::cout << "║   BẢNG ĐIỀU KHIỂN    ║"; break;
+            case 1:  std::cout << "╠══════════════════════╣"; break;
+            case 2:  std::cout << "║ [A/←]   : Trái       ║"; break;
+            case 3:  std::cout << "║ [D/→]   : Phải       ║"; break;
+            case 4:  std::cout << "║ [W/↑]   : Xoay       ║"; break;
+            case 5:  std::cout << "║ [X/↓]   : Rơi nhanh  ║"; break;
+            case 6:  std::cout << "║ [Space] : Rơi ngay   ║"; break;
+            case 7:  std::cout << "║ [P]     : Tạm dừng   ║"; break;
+            case 8:  std::cout << "║ [Q]     : Thoát      ║"; break;
+            case 9:  std::cout << "╠══════════════════════╣"; break;
+            case 10: std::cout << "║      BẢNG ĐIỂM       ║"; break;
+            case 11: std::cout << "╠══════════════════════╣"; break;
+            case 12: std::cout << "║ K.Lục: " << RED    << padLeft(score.highScore, 13) << RESET << " ║"; break;
+            case 13: std::cout << "║ Điểm : " << YELLOW << padLeft(score.score, 13)     << RESET << " ║"; break;
+            case 14: std::cout << "║ Hàng : " << GREEN  << padLeft(score.totalLines, 13) << RESET << " ║"; break;
+            case 15: std::cout << "╠══════════════════════╣"; break;
+            case 16: 
+                if (paused) std::cout << "║ TT: " << RED << "   TẠM DỪNG    " << RESET << " ║";
+                else        std::cout << "║ TT: Đang chơi        ║"; 
+                break;
+            case 17: std::cout << "║ Combo:               ║"; break;
+            case 18: std::cout << "║ 1 hàng =  36đ        ║"; break;
+            case 19: std::cout << "╚══════════════════════╝"; break;
+            default: std::cout << "                        "; break;
+        }
+
+        // Vẽ Khung Block Tiếp Theo (Phải)
+        std::cout << "   ";
+        switch(i) {
+            case 0:  std::cout << "║  BLOCK   ║"; break;
+            case 1:  std::cout << "║          ║"; break;
+            case 2: case 3: case 4: case 5: {
+                int r = i - 2;
+                std::cout << "║ ";
+                for (int j = 0; j < 4; j++) {
+                    if (nextBlock.shape[r][j] != ' ') {
+                        applyColor(nextBlock.shape[r][j]);
+                        std::cout << "[]";
+                        std::cout << RESET;
+                    } else {
+                        std::cout << "  ";
+                    }
+                }
+                std::cout << " ║";
+                break;
+            }
+            case 6:  std::cout << "╚══════════╝"; break;
+            default: std::cout << "            "; break;
+        }
+        std::cout << "\n";
+    }
 }
 
 void CRenderer::drawGameOver(const CScoreManager& score) const {
-    // TODO
+    gotoxy(0, BOARD_H + 2);
+    std::cout << WHITE << "╔══════════════════════════════╗\n";
+    std::cout << "║        GAME OVER!            ║\n";
+    std::cout << "║  Kỷ lục:       " << RED    << padLeft(score.highScore, 8) << WHITE << "      ║\n";
+    std::cout << "║  Điểm của bạn: " << YELLOW << padLeft(score.score, 8)     << WHITE << "      ║\n";
+    std::cout << "║  Số hàng xoá:  " << GREEN  << padLeft(score.totalLines, 8)<< WHITE << "      ║\n";
+    std::cout << "╚══════════════════════════════╝" << RESET << "\n";
 }
